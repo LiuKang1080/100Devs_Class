0a544338c9b30b144b6ac6a306373a8ee1cfccd7
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-02-26 19:58:13 -0500

    Completed JavaScript tasks in Class_14

--- Class_14/bring-it-home-practice/js/main.js
+++ Class_14/bring-it-home-practice/js/main.js
@@ -1,14 +1,40 @@
 // *Variables*
 // Create a variable and console log the value
+let x = 25;
+console.log(x);
 
 // Create a variable, add 10 to it, and alert the value
+let y = 100;
+y += 10;
+
+alert(y);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+function subtract(a, b, c, d) {
+    alert(a - b - c - d);
+}
 
 // Create a function that divides one number by another and returns the remainder
+function modulo(x, y) {
+    return(x % y);
+}
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+function addition(x, y) {
+    if ((x + y) > 50) {
+        alert("Jumanji");
+    } else {
+        console.log(x + y);
+    }
+}
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+function multiply(x, y, z) {
+    let prod = x * y * z;
+
+    if (prod % 3 === 0) {
+        alert("ZEBRA");
+    }
+}
--- Class_14/simple-functions/js/main.js
+++ Class_14/simple-functions/js/main.js
@@ -1,5 +1,24 @@
 //create a function that adds two numbers and alerts the sum
+function addition(x, y) {
+    alert(x + y);
+}
 
 //create a function that multiplys three numbers and console logs the product
+function multiply(x, y, z) {
+    console.log(x * y * z);
+}
 
 //create a function that divides two numbers and returns the ???
+function division(x, y) {
+    /*
+        - It will be x divided by y.
+        - Check to see if y is 0, if it's zero, log out division by zero error
+    */
+
+    if (y === 0) {
+        console.log("Division by zero error!");
+        alert("Division by zero error!");
+    }
+
+    return x / y;
+}
