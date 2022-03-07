b2fafbfca7ba65bc080109f5e48a30080b07052d
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-03-06 19:17:48 -0500

    Added some code to finish tasks and challenges in Class_16

--- Class_16/bring-it-on/js/main.js
+++ Class_16/bring-it-on/js/main.js
@@ -1,15 +1,28 @@
 // *Variables*
 // Create a variable and console log the value
+let x = 10;
 
 // Create a variable, add 10 to it, and alert the value
+x += 10;
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+function subtract(a, b, c, d) {
+    alert(a - b - c - d);
+}
 
 // Create a function that divides one number by another and returns the remainder
+function division(x, y) {
+    if (y !== 0) {
+        return (x % y);
+    } else {
+        console.log("Error: Division by zero!");
+    }
+}
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
 
