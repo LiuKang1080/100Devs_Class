c4ce831a8d7ee02f69f4f39afac4be534843c330
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-03-27 18:26:15 -0400

    Adjusted some comments and code in turbo-review/main.js

--- Class_22/turbo-review/js/main.js
+++ Class_22/turbo-review/js/main.js
@@ -9,44 +9,44 @@ console.log(fav_drink);
 let str = "Testing Hello World!";
 
 if (str.search("apple") < 0) {
-    console.log("The word 'apple' is not in the sentence.");
+	console.log("The word 'apple' is not in the sentence.");
 } else {
-    console.log("The word 'apple' is in the sentence!");
+	console.log("The word 'apple' is in the sentence!");
 }
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
 function rock_paper_scissors() {
-    let choice = Math.floor(Math.random() * 3);
+	let choice = Math.floor(Math.random() * 3);
 
-    if (choice === 0) {
-        return "rock";
-    } else if (choice === 1) {
-        return "paper";
-    } else if (choice === 2) {
-        return "scissors";
-    }
+	if (choice === 0) {
+		return "rock";
+	} else if (choice === 1) {
+		return "paper";
+	} else if (choice === 2) {
+		return "scissors";
+	}
 }
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
 function play_game(user_choice) {
-    user_choice = user_choice.toLowerCase();
+	user_choice = user_choice.toLowerCase();
 
-    // get result from bot
-    bot_choice = rock_paper_scissors();
+	// get result from bot
+	bot_choice = rock_paper_scissors();
 
-    if (
-        ( (user_choice === "rock") && (bot_choice === "scissors") ) ||
-        ( (user_choice === "paper") && (bot_choice === "rock") ) ||
-        ( (user_choice === "scissors") && (bot_choice === "paper")) ) {
+	if (
+		( (user_choice === "rock") && (bot_choice === "scissors") ) ||
+		( (user_choice === "paper") && (bot_choice === "rock") ) ||
+		( (user_choice === "scissors") && (bot_choice === "paper")) ) {
 
-        console.log("You Win!");
-    } else if (user_choice === bot_choice) {
-        console.log("You Tied!");
-    } else {
-        console.log("You Lose!");
-    }
+		console.log("You Win!");
+	} else if (user_choice === bot_choice) {
+		console.log("You Tied!");
+	} else {
+		console.log("You Lose!");
+	}
 }
 
 play_game("rock");
@@ -54,9 +54,9 @@ play_game("rock");
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
 function play_multiple_times(user_choice_array) {
-    user_choice_array.forEach( choice => {
-        play_game(choice);
-    });
+	user_choice_array.forEach( choice => {
+		play_game(choice);
+	});
 }
 
 let user_choice_array = ["rock", "paper", "scissors"];
