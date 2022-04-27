59b64d94327680f6597868e760675b10c802be86
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-04-26 23:11:52 -0400

    Finished taks in Boulder_Badge and Cascade_Badge in Class_29

--- Class_29/boulder-badge/js/main.js
+++ Class_29/boulder-badge/js/main.js
@@ -1,8 +1,60 @@
 // Variables
-//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
+/*
+You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least
+once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and
+weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your 
+pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that
+sums all the rare candies you would need. 
+*/
+let bulbasaur = 5;
+let caterpie = 1;
+let weedle = 1;
+let totalCandies = 0;
+
+function add_candies(bulbasaur_level, caterpie_level, weedle_level) {
+    totalCandies += bulbasaur_level - bulbasaur;
+    totalCandies += caterpie_level - caterpie;
+    totalCandies += weedle_level - weedle;
+
+    return totalCandies;
+}
+
+console.log(add_candies(16, 7, 7));
+
 
 //Conditionals And Functions
-//You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+/*
+You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one 
+function that converts a Fahrenheit value to Celcius and another fuction that tells you whether or not charmander can battle
+*/
+function temp_converter(fahrenheit) {
+    // convert Fahrenheit value to a Celcius value
+    // returns celcius value
+    return (fahrenheit - 32) / 1.8000;
+}
+
+function charmander_check(temp) {
+    if (temp_converter(temp) > 0) {
+        console.log("Charmander can battle!");
+    } else {
+        console.log("Charmander cannot battle");
+    }
+}
+
 
 //Loops
-//You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
+/*
+You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console
+"Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party.
+*/
+let str = "";
+
+function send_pikachu(party_size) {
+    for (let i = 1; i <= party_size; i++ ) {
+        str += "Pikachu I choose you! ";
+    }
+
+    console.log(str);
+}
+
+send_pikachu(6);
--- Class_29/cascade-badge/js/main.js
+++ Class_29/cascade-badge/js/main.js
@@ -1,7 +1,38 @@
 //Arrays
-//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+/*
+Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list
+and prints it to the console.
+*/
+function reverse_array(array) {
+    console.log(array.reverse());
+}
 
-//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+/*
+Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element
+in a is strictly greater than the sum of the cubes of each element in b.
+*/
+function array_sum_check(arr_a, arr_b) {
+    let arr_a_sum = 0;
+    let arr_b_sum = 0;
+
+    // loop through array a:
+    for (let i = 0; i < arr_a.length; i++) {
+        arr_a_sum += (Math.pow(arr_a[i], 2));
+    }
+
+    // loop through array b:
+    for (let i = 0; i < arr_b.length; i++) {
+        arr_b_sum += (Math.pow(arr_b[i], 3));
+    }
+
+    if (arr_a_sum > arr_b_sum) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
