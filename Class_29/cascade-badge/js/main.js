4d276ae5af33d3dcd677a152f669c7cf0d62a202
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-04-27 20:59:11 -0400

    Adjusted code for all of the badges.

--- Class_29/cascade-badge/js/main.js
+++ Class_29/cascade-badge/js/main.js
@@ -39,9 +39,50 @@ function array_sum_check(arr_a, arr_b) {
 }
 
 
-//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
+function array_multiple(arr) {
+    // declare variables
+    let array_multiple = [];
 
-//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+    // loop through the array
+    for (let i = 0; i < arr.length; i++) {
+        // if the element is completely divisible by the current index, add that element to array_multiple
+        if (arr[i] % i === 0) {
+            array_multiple.push(arr[i]);
+        }
+    }
+
+    return array_multiple;
+}
+
+console.log(array_multiple([22, -6, 32, 82, 9, 25]));
+
+
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers. Return your answer
+as a number.
+*/
+function array_convert_sum(arr) {
+    // declare variables
+    let total = 0;
+
+    // loop through the array, convert strings into number, and then add it to the total
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof(arr[i]) === "string") {
+            // convert current element into number, and then add it to the total
+            total += Number(arr[i]);
+        } else {
+            // it's already a number, add it to the total
+            total += arr[i];
+        }
+    }
+
+    return total
+}
+
+/*
+    - We could have used the built in methods like map(), filter(), and reduce()
+*/
